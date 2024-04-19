46772a91b613b383b1596fb4d9467ead9a0097bd
Carlo M <carlo992@gmail.com>
2024-04-19 09:52:32 -0600

    Reverted to previous version

--- src/core.js
+++ src/core.js
@@ -27,11 +27,7 @@ const mainInstrument = undefined
 
 // 11. Using an index on the persons friends array, set the bestFriend variable below
 // to the name of the first friend in the array
-const bestFriend = 'chris'
-const message = 'Best friend is:'
-console.log(message, bestFriend)
-console.log('Best friend is:', bestFriend)
-console.log('Hi', 20, false, { name: 'carlo' })
+const bestFriend = undefined
 
 // Don't change the code below this line
 module.exports = {
